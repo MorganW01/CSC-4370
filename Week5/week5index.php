7722cc599489916d940e4b65aceacc7f411f5886
Morgan <morganw2001@gmail.com>
2022-02-13 17:35:04 -0500

    fixed lecture example!

--- Week5/week5index.php
+++ Week5/week5index.php
@@ -4,82 +4,79 @@ define( 'DB_USER', 'mwarren15');
 define( 'DB_PASSWORD', 'mwarren15');
 define( 'DB_HOST', 'localhost');
 
-function DeleteCarEntry($id){
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
+function DeleteCarEntry($id) {
+
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	// Check connection
+	if (!$conn) {
+	  die("Connection failed: " . mysqli_connect_error());
+	}
+	
     $del = "DELETE FROM Cars WHERE id = '$id' ";
-    
-    $result = $conn->query($del);
-    
-    mysqli_close($conn);
-
-
+	
+	$result = $conn->query($del);
+	
+	mysqli_close($conn);
 }
 
-function InsertMake($make){
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
+function InsertMake($make) {
+
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	// Check connection
+	if (!$conn) {
+	  die("Connection failed:" . mysqli_connect_error());
+	}
+	
     $insert = "INSERT INTO Cars SET make = '$make' ";
-    
-    $result = $conn->query($insert);
-    
-    mysqli_close($conn);
-
+	
+	$result = $conn->query($insert);
+	
+	mysqli_close($conn);
 }
 
-function ShowCars(){
-    // Create connection
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    // Check connection
-    if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-    }
+
+
+function ShowCars() {
+// Create connection
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	// Check connection
+	if (!$conn) {
+	  die("Connection failed:" . mysqli_connect_error());
+	}
 
     $sql = "SELECT id, make FROM Cars";
-    $result = mysqli_query($conn, $sql);
+	$result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        $delurl = "[<a href='https://codd.cs.gsu.edu/~mwarren15/week5.php?cmd=delete&id={$row['id']}'>delete</a>]";
-        echo "id: " . $row["id"]. " - Make: " . $row["make"]. " $delurl<br>";
-    }
-    } else {
-    echo "0 results";
-    }
+	if (mysqli_num_rows($result) > 0) {
+	  // output data of each row
+	  while($row = mysqli_fetch_assoc($result)) {
+	  	$delurl = "[<a href='https://codd.cs.gsu.edu/~mwarren15/week5index.php?cmd=delete&id={$row['id']}'>delete</a>]";
+		echo "id:" . $row["id"]. "– Make: "  . $row["make"]. " $delurl<br>";
+	  }
+	} else {
+	  echo "0 results";
+	}
 
-    mysqli_close($conn);
-
+	mysqli_close($conn);
 }
 
 ?>
 
 <form method="get">
-    Car Make: <input type="text" name="carmake"><br>
-    <input type="submit" value="Submit">
-
+  Car Make: <input type="text" name="carmake"><br>
+  <input type="submit" value="Submit">
 </form>
 
 <?php
-if($_GET['carmake'] != ''){
-    InsertMake($_GET['carmake']);
-
-}
-if($_GET['cmd'] == 'delete'){
-    $id = $_GET['id'];
-    DeleteCarEntry($id);
-
-
+if($_GET['carmake'] != '') {
+	InsertMake($_GET['carmake']);
 }
 
+if($_GET['cmd'] == 'delete') {
+	$id = $_GET['id'];
+	DeleteCarEntry($id);
+}
 
 ShowCars();
 ?>
+	
