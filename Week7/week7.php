6cd83af873d59fc97e8010273f7d7da374c158b3
Morgan <morganw2001@gmail.com>
2022-03-01 12:12:11 -0500

    complete week 7

---
+++ Week7/week7.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="./jquery/jquery-3.6.0.min.js"></script>
+    <head>
+    <body>
+        This is an ajax example
+        <form onsubmit ="return(insertInfo())">
+            <input type = text id = firstname>
+            <input type = text id = lastname>
+            <input type = text id = telephone>
+            <input type=submit value=submit>
+        </form>
+
+
+        <div id=showinfo ></div>
+        <script>
+            function insertInfo(){
+                val1 = $("#firstname").val();
+                val2 = $("#lastname").val();
+                val3 = $("#telephone").val();
+
+                $.get("./week7ajax.php", {"cmd": "create", "firstname" : val1, "lastname" : val2, "telephone" : val3}, function(data) {
+                    $("#showinfo").html(data); 
+                } );
+                return(false);
+            }
+
+            function deleteInfo(id){
+                $.get("./week7ajax.php", {"cmd": "delete", "id" : id}, function(data) {
+                    $("#showinfo").html(data); 
+                } );
+                return(false);
+            }
+
+            function showinfo() {
+                $.get("./week7ajax.php", {"cmd": ""}, function(data) {
+                    $("#showinfo").html(data);
+                    
+                } );
+                return (false);
+            }
+            showinfo();
+            
+
+        </script>
+    <body>
+</html>
+
+
+
+
--- Week7/week7ajax.php
+++ Week7/week7ajax.php
@@ -10,6 +10,20 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+
+function DeleteInfo($id) {
+    global $conn;
+    $del = "DELETE FROM People WHERE id = '$id' ";
+    $result = $conn->query($del);
+}
+
+
+function InsertInfo($firstname, $lastname, $telephone) {
+    global $conn;
+    $insert = "INSERT INTO People SET firstname = '$firstname', lastname = '$lastname', telephone = '$telephone' ";
+    $result = $conn->query($insert);    
+}
+
 function ShowInfo() {
     global $conn;
     
@@ -19,13 +33,32 @@ function ShowInfo() {
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-            $delurl = "[<a href='https://codd.cs.gsu.edu/~mwarren15/week5.php?cmd=delete&id={$row['id']}'>delete</a>]";
-        echo"id: " . $row["id"]. " First Name: "  . $row["firstname"]. " Last Name: "  . $row["lastname"]." Telephone: "  . $row["telephone"]. " $delurl<br>";
+            $id = $row["id"];
+            $delurl = "[<a href='' onclick=return(deleteInfo('$id'))>delete</a>]";
+            echo"id: " . $row["id"]. " First Name: "  . $row["firstname"]. " Last Name: "  . $row["lastname"]." Telephone: "  . $row["telephone"]. " $delurl<br>";
         }
     } else {
         echo "0 results";
     }
         
+}
+
+$cmd = $_GET['cmd'];
+
+if ($cmd =='create'){
+    if($_GET['firstname'] != '') {
+        if($_GET['lastname'] != '') {
+            if($_GET['telephone'] != '') {
+                InsertInfo($_GET['firstname'], $_GET['lastname'], $_GET['telephone']);
+            }
+        }   
+    }   
+}
+
+  
+else if($cmd == 'delete') {
+    $id = $_GET['id'];
+    DeleteInfo($id);
 }
 
 ShowInfo();
